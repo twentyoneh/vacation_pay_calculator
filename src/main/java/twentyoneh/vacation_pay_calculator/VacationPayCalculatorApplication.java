72f412f7a45802058c8e04d6508a5748078e02b0
Kalinin Dmitry <didiwot@yandex.ru>
2025-04-01 17:03:01 +0300

    fist step

---
+++ src/main/java/twentyoneh/vacation_pay_calculator/VacationPayCalculatorApplication.java
@@ -0,0 +1,20 @@
+package twentyoneh.vacation_pay_calculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class VacationPayCalculatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VacationPayCalculatorApplication.class, args);
+	}
+	@GetMapping("/hello")
+    public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+      return String.format("Hello %s!", name);
+    }
+}
